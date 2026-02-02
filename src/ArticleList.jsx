aac5afa3baa3c0a10473e9738ceee88e9acd2c2b
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-02-02 13:30:37 +0000

    ajout du filtre de recherche

--- src/ArticleList.jsx
+++ src/ArticleList.jsx
@@ -1,13 +1,40 @@
 import Data from './Data.json'
-import Card from './Card'
+import {useState} from 'react'
+ import Card from './Card'
 
 function ArticleList(props){
+    
+    const [SelectedCategory, setSelectedCategory] = useState("Toutes");
+    
+    const filtreArticle = Data.filter((article) => {
+        if(SelectedCategory === "Toutes"){
+            return true;
+        }else {
+            return article.category === SelectedCategory
+        }
+    })
+    
+   
 return(
+    <>
+    <div className = "list-btn">
+    <button onClick = {() => setSelectedCategory("Toutes")}> Toutes </button>
+    <button onClick = {() => setSelectedCategory("Tutoriel")}> Tutoriel </button>
+    <button onClick = {() => setSelectedCategory("Animation")}>Animation</button>
+    <button onClick = {() => setSelectedCategory("Design")}>Design</button>
+    <button onClick = {() => setSelectedCategory("Théorie")}>Théorie</button>
+    <button onClick = {() => setSelectedCategory("Outils")}>Outils</button>
+    <button onClick = {() => setSelectedCategory("Technique")}>Technique</button>
+    </div>
+    
     <div className="blog-grid">
-    {Data.map((article) => (
+    
+    
+    {filtreArticle.map((article) => (
         <Card key = {article.id} article = {article} setSelectedArticle = {props.setSelectedArticle} />
 ))}
     </div>
+    </>
     
     ) 
     
